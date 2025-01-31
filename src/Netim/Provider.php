10aa89661bf3609e5a1811a20732dc614a6e8335
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2025-01-31 09:15:45 +0000

    empty string epp code on transfer in

--- src/Netim/Provider.php
+++ src/Netim/Provider.php
@@ -255,7 +255,7 @@ class Provider extends DomainNames implements ProviderInterface
 
             $result = $this->client()->domainTransferIn(
                 $domain,
-                $params->epp_code,
+                $params->epp_code ?? '',
                 $registrant,
                 $admin,
                 $tech,
@@ -628,6 +628,9 @@ class Provider extends DomainNames implements ProviderInterface
         return $normalizedContact->to_array();
     }
 
+    /**
+     * @throws NetimAPIException
+     */
     protected function getContactInfo($idContact): array
     {
         $contact = $this->client()->contactInfo($idContact);

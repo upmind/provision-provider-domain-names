524f26d631ad111c160fb260173ed77eb749be62
Harry Lewis <harry@upmind.com>
2025-04-17 17:17:42 +0100

    Add 5 second connection timeout to SynergyWholesale SOAP service

--- src/SynergyWholesale/Provider.php
+++ src/SynergyWholesale/Provider.php
@@ -322,11 +322,16 @@ class Provider extends DomainNames implements ProviderInterface
         $hostname = $this->configuration->custom_api_hostname ?: 'api.synergywholesale.com';
 
         try {
+            // apparently the only way to set the actual timeout for SoapClient
+            $defaultTimeout = ini_set("default_socket_timeout", 5);
+
             $client = new SoapClient(sprintf('https://%s/?wsdl', $hostname));
 
             return $this->api = new SynergyWholesaleApi($client, $this->configuration, $this->getLogger());
         } catch (Throwable $e) {
             $this->handleException($e);
+        } finally {
+            ini_set("default_socket_timeout", $defaultTimeout);
         }
     }
 

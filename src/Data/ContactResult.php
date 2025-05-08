b1a481ad5dbc83ac2a241eb980cc5c9f5529b98c
Harry Lewis <harry@upmind.com>
2025-05-08 17:23:37 +0100

    Relax ContactResult validation rules to allow empty return fields

--- src/Data/ContactResult.php
+++ src/Data/ContactResult.php
@@ -10,36 +10,32 @@ use Upmind\ProvisionBase\Provider\DataSet\Rules;
 /**
  * Contact data.
  *
- * @property-read string $name Name of new contact
- * @property-read string $organisation Organisation of new contact
- * @property-read string $email Email of new contact
+ * @property-read string|int|null $id ID of contact
+ * @property-read string|null $name Name of new contact
+ * @property-read string|null $organisation Organisation of new contact
+ * @property-read string|null $email Email of new contact
  * @property-read string|null $phone Phone of new contact
- * @property-read string $address1 Full address of new contact
- * @property-read string $city City of new contact
+ * @property-read string|null $address1 Full address of new contact
+ * @property-read string|null $city City of new contact
  * @property-read string|null $state
  * @property-read string|null $postcode Postcode of new contact
  * @property-read string|null $country_code Country of new contact
- * @property-read string|null $type Type of new contact
- * @property-read string|null $password Password of new contact
- * @property-read string|int|null $id ID of contact
  */
 class ContactResult extends ResultData
 {
     public static function rules(): Rules
     {
         return new Rules([
+            'id' => ['nullable'],
             'name' => ['nullable', 'string'],
             'organisation' => ['nullable', 'string'],
-            'email' => ['required', 'email'],
-            'phone' => ['nullable', 'string', 'international_phone'],
-            'address1' => ['required', 'string'],
-            'city' => ['required', 'string'],
+            'email' => ['nullable', 'string'],
+            'phone' => ['nullable', 'string'],
+            'address1' => ['nullable', 'string'],
+            'city' => ['nullable', 'string'],
             'state' => ['nullable', 'string'],
             'postcode' => ['nullable', 'string'],
             'country_code' => ['nullable', 'string', 'size:2', 'country_code'],
-            'type' => ['nullable', 'string'],
-            'password' => ['nullable', 'string'],
-            'id' => ['nullable'],
         ]);
     }
 

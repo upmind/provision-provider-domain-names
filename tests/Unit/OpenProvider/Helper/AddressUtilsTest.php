644fa1cbfba26e882033af835867553c48701121
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2025-07-05 13:50:55 +0100

    Add method & test to sanitise values for city or state

--- tests/Unit/OpenProvider/Helper/AddressUtilsTest.php
+++ tests/Unit/OpenProvider/Helper/AddressUtilsTest.php
@@ -9,14 +9,22 @@ use Upmind\ProvisionProviders\DomainNames\OpenProvider\Helper\AddressUtils;
 
 class AddressUtilsTest extends TestCase
 {
-    public function test_postcode_sanitisation_does_not_change_alpanumeric_value_with_spaces(): void
+    public function test_sanitise_city_or_state_removes_non_alphanumeric_characters_that_are_not_allowed(): void
+    {
+        // Use all the allowed special characters
+        $city = 'Ne_w Yo-rk,!@\'#$"%^&*().+';
+
+        $this->assertEquals('Ne_w Yo-rk,\'.', AddressUtils::sanitiseCityOrState($city));
+    }
+
+    public function test_sanitise_postcode_does_not_change_alpanumeric_value_with_spaces(): void
     {
         $postCode = '1234 AB 5678';
 
         $this->assertEquals($postCode, AddressUtils::sanitisePostCode($postCode));
     }
 
-    public function test_postcode_sanitisation_removes_non_alphanumeric_characters_from_value(): void
+    public function test_sanitise_postcode_removes_non_alphanumeric_characters_from_value(): void
     {
         $postCode = '1234 AB 5678!@#$%^&*()_+21=23 AB 4567';
 

59ff9faa50ee18cf7463681cc9afedf74455c8cb
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2025-07-18 16:46:32 +0100

    set moniker about provider method

--- src/Moniker/Provider.php
+++ src/Moniker/Provider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Upmind\ProvisionProviders\DomainNames\Moniker;
 
+use Upmind\ProvisionBase\Provider\DataSet\AboutData;
 use Upmind\ProvisionProviders\DomainNames\CentralNicReseller\Helper\CentralNicResellerApi;
 use Upmind\ProvisionProviders\DomainNames\CentralNicReseller\Provider as CentralNicResellerProvider;
 use Upmind\ProvisionProviders\DomainNames\Moniker\Data\Configuration;
@@ -18,6 +19,17 @@ class Provider extends CentralNicResellerProvider
     public function __construct(Configuration $configuration)
     {
         parent::__construct($configuration);
+    }
+
+    public static function aboutProvider(): AboutData
+    {
+        return AboutData::create()
+            ->setName('Moniker')
+            ->setDescription(
+                'Moniker, Built For Domain Investors. '
+                . 'Buy, sell, manage, and monetize domain names.'
+            )
+            ->setLogoUrl('https://api.upmind.io/images/logos/provision/moniker-logo_2x.png');
     }
 
     /**

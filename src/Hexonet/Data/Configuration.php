927351fb51b95a4d8b839407b0629760f81c390e
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2025-07-19 10:35:51 +0100

    Allow setting migrated service in hexonet config

--- src/Hexonet/Data/Configuration.php
+++ src/Hexonet/Data/Configuration.php
@@ -12,6 +12,7 @@ use Upmind\ProvisionBase\Provider\DataSet\Rules;
  *
  * @property-read string $username
  * @property-read string $password
+ * @property-read string|null $migrated
  * @property-read bool|null $sandbox
  * @property-read bool|null $debug Whether or not to enable debug logging
  */
@@ -22,6 +23,7 @@ class Configuration extends DataSet
         return new Rules([
             'username' => ['required', 'string', 'min:3'],
             'password' => ['required', 'string', 'min:6', 'max:16'],
+            'migrated' => ['nullable', 'string', 'in:not-migrated,centralnic-reseller,moniker'],
             'sandbox' => ['nullable', 'boolean'],
             'debug' => ['nullable', 'boolean'],
         ]);

6e84e71b97878a4ea76ff05de1db067070a6d13a
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2025-08-12 16:43:39 +0100

    always throw provision function error in async request

--- src/OpenSRS/Helper/OpenSrsApi.php
+++ src/OpenSRS/Helper/OpenSrsApi.php
@@ -219,6 +219,8 @@ class OpenSrsApi
             }
 
             return self::parseResponseData($result);
+        })->otherwise(function (Throwable $t) {
+            static::errorResult($t->getMessage(), [], [], $t);
         });
     }
 

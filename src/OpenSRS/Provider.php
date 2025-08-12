6410fb424dce274fe3243ffc8470ee953fc961aa
Dan <42817204+domainregistrar@users.noreply.github.com>
2025-08-12 09:48:59 +0100

    Organisation field should now be optional
    https://opensrs.com/blog/what-icanns-updated-registration-data-policy-rdp-means-for-opensrs-resellers/

--- src/OpenSRS/Provider.php
+++ src/OpenSRS/Provider.php
@@ -208,7 +208,7 @@ class Provider extends DomainNames implements ProviderInterface
 
                 $contacts[$type] = array_filter([
                     'country' => Utils::normalizeCountryCode($contactParams->country_code),
-                    'org_name' => $contactParams->organisation ?: $contactParams->name,
+                    'org_name' => $contactParams->organisation,
                     'phone' => Utils::internationalPhoneToEpp($contactParams->phone),
                     'postal_code' => $contactParams->postcode,
                     'city' => $contactParams->city,
@@ -340,7 +340,7 @@ class Provider extends DomainNames implements ProviderInterface
             $contacts[$type] = [
                 'country' => Utils::normalizeCountryCode($contactParams->country_code),
                 'state' => Utils::stateNameToCode($contactParams->country_code, $contactParams->state),
-                'org_name' => $contactParams->organisation ?: $contactParams->name,
+                'org_name' => $contactParams->organisation,
                 'phone' => Utils::internationalPhoneToEpp($contactParams->phone),
                 'postal_code' => $contactParams->postcode,
                 'city' => $contactParams->city,

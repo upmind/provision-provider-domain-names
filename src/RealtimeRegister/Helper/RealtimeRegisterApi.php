530e6a3623a0500a372dc9cdb18fc7b04390d4b4
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2025-10-24 10:24:21 +0300

    Used the correct class to check whether no epp code transfer is supported

--- src/RealtimeRegister/Helper/RealtimeRegisterApi.php
+++ src/RealtimeRegister/Helper/RealtimeRegisterApi.php
@@ -18,7 +18,7 @@ use Upmind\ProvisionProviders\DomainNames\Data\ContactParams;
 use Upmind\ProvisionProviders\DomainNames\Data\DacDomain;
 use Upmind\ProvisionProviders\DomainNames\Data\DomainNotification;
 use Upmind\ProvisionProviders\DomainNames\Data\NameserversResult;
-use Upmind\ProvisionProviders\DomainNames\Helper\Tlds\RequiresTransferAuthCode;
+use Upmind\ProvisionProviders\DomainNames\Helper\Tlds\NoEppCodeTransfer;
 use Upmind\ProvisionProviders\DomainNames\Helper\Utils;
 use Upmind\ProvisionProviders\DomainNames\RealtimeRegister\Data\Configuration;
 
@@ -536,7 +536,7 @@ class RealtimeRegisterApi
         }
 
         // Remove epp/auth code if not supported on transfer.
-        if (!RequiresTransferAuthCode::tldRequiresAuthCode($domainName)) {
+        if (NoEppCodeTransfer::tldIsSupported($domainName)) {
             unset($body['epp_code']);
         }
 

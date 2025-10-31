1e337f5713620f0b3c8188a1544957c6308a96c9
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2025-10-31 13:30:22 +0200

    Update config methods

--- src/Netistrar/Data/Configuration.php
+++ src/Netistrar/Data/Configuration.php
@@ -8,10 +8,10 @@ use Upmind\ProvisionBase\Provider\DataSet\DataSet;
 use Upmind\ProvisionBase\Provider\DataSet\Rules;
 
 /**
- * Example configuration.
+ * Netistrar configuration.
  *
- * @property-read string $username Login id
- * @property-read string $api_token API token
+ * @property-read string $api_key API Key
+ * @property-read string $api_secret API Secret
  * @property-read bool|null $sandbox Make API requests against the sandbox environment
  */
 class Configuration extends DataSet
@@ -23,5 +23,20 @@ class Configuration extends DataSet
             'api_secret' => ['required', 'string', 'min:10'],
             'sandbox' => ['nullable', 'boolean'],
         ]);
+    }
+
+    public function getApiKey(): string
+    {
+        return $this->api_key;
+    }
+
+    public function getApiSecret(): string
+    {
+        return $this->api_secret;
+    }
+
+    public function isSandbox(): bool
+    {
+        return (bool) $this->sandbox;
     }
 }

215285b6772d3257e54f361cefce65a86fb6266e
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2025-12-11 11:44:10 +0000

    Update enom provider fully with new updateContact method

--- src/Data/Enums/ContactType.php
+++ src/Data/Enums/ContactType.php
@@ -4,10 +4,22 @@ declare(strict_types=1);
 
 namespace Upmind\ProvisionProviders\DomainNames\Data\Enums;
 
+use Upmind\ProvisionProviders\DomainNames\Enom\Helper\EnomApi;
+
 enum ContactType: string
 {
     case REGISTRANT = 'registrant';
     case ADMIN = 'admin';
     case BILLING = 'billing';
     case TECH = 'tech';
+
+    public function providerEnomValue(): string
+    {
+        return match ($this) {
+            self::REGISTRANT => mb_strtolower(EnomApi::CONTACT_TYPE_REGISTRANT),
+            self::ADMIN => mb_strtolower(EnomApi::CONTACT_TYPE_ADMIN),
+            self::BILLING => mb_strtolower(EnomApi::CONTACT_TYPE_BILLING),
+            self::TECH => mb_strtolower(EnomApi::CONTACT_TYPE_TECH),
+        };
+    }
 }

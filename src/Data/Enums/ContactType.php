009792f31522c759ddf15398e330c5031d3a56a9
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2025-12-11 14:34:13 +0000

    Update SynergyWholesale provider with new updateContact method

--- src/Data/Enums/ContactType.php
+++ src/Data/Enums/ContactType.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Upmind\ProvisionProviders\DomainNames\Data\Enums;
 
 use Upmind\ProvisionProviders\DomainNames\Enom\Helper\EnomApi;
+use Upmind\ProvisionProviders\DomainNames\SynergyWholesale\Helper\SynergyWholesaleApi;
 
 enum ContactType: string
 {
@@ -18,6 +19,11 @@ enum ContactType: string
         return $this->value === $value;
     }
 
+    public function isNotEqualValue(string $value): bool
+    {
+        return $this->isEqualValue($value) === false;
+    }
+
     public function providerEnomValue(): string
     {
         return match ($this) {
@@ -25,6 +31,16 @@ enum ContactType: string
             self::ADMIN => mb_strtolower(EnomApi::CONTACT_TYPE_ADMIN),
             self::BILLING => mb_strtolower(EnomApi::CONTACT_TYPE_BILLING),
             self::TECH => mb_strtolower(EnomApi::CONTACT_TYPE_TECH),
+        };
+    }
+
+    public function providerSynergyWholesaleValue(): string
+    {
+        return match ($this) {
+            self::REGISTRANT => mb_strtolower(SynergyWholesaleApi::CONTACT_TYPE_REGISTRANT),
+            self::ADMIN => mb_strtolower(SynergyWholesaleApi::CONTACT_TYPE_ADMIN),
+            self::BILLING => mb_strtolower(SynergyWholesaleApi::CONTACT_TYPE_BILLING),
+            self::TECH => mb_strtolower(SynergyWholesaleApi::CONTACT_TYPE_TECH),
         };
     }
 }

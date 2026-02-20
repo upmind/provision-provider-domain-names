164e397bcc14252b0239069ed3fc1748988855ad
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2026-02-20 17:28:23 +0000

    change comment

--- src/OpenProvider/Helper/AddressUtils.php
+++ src/OpenProvider/Helper/AddressUtils.php
@@ -26,7 +26,7 @@ class AddressUtils
 
     public static function sanitisePostCode(string $value): string
     {
-        // Remove any non-alphanumeric and space characters from the post code
+        // Remove any characters that are not alphanumeric or a space from the post code
         $sanitised = preg_replace('/[^a-zA-Z0-9 ]/', '', $value);
 
         // Return the sanitised post code

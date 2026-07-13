c0f39a60fd8114c2488b1f71c7a4239bf2dc6f61
RoussKS <35-roussks@users.noreply.git.upmind.io>
2026-07-13 14:58:11 +0100

    migrate soap request sto separate api class

--- src/DomainNameApi/Data/DomainNameApiConfiguration.php
+++ src/DomainNameApi/Data/DomainNameApiConfiguration.php
@@ -8,7 +8,7 @@ use Upmind\ProvisionBase\Provider\DataSet\DataSet;
 use Upmind\ProvisionBase\Provider\DataSet\Rules;
 
 /**
- * DomainNamemApi configuration
+ * DomainNameApi configuration
  *
  * @property-read string $username Username
  * @property-read string $password Password
@@ -25,5 +25,10 @@ class DomainNameApiConfiguration extends DataSet
             'sandbox' => ['boolean'],
             'debug' => ['boolean'],
         ]);
+    }
+
+    public function isSandbox(): bool
+    {
+        return $this->sandbox !== null && $this->sandbox === true;
     }
 }

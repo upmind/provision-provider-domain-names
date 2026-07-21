bfeb930ad8c2a54f3d1104646c7619420898cfee
RoussKS <35-roussks@users.noreply.git.upmind.io>
2026-07-21 11:52:44 +0100

    Add reseller setup for Gandi configuration

--- src/Gandi/Data/Configuration.php
+++ src/Gandi/Data/Configuration.php
@@ -13,15 +13,27 @@ use Upmind\ProvisionBase\Provider\DataSet\Rules;
  * @property-read string $api_token API token
  * @property-read bool|null $sandbox Make API requests against the sandbox environment
  * @property-read string|null $sharing_id Organization/reseller billing ID
+ * @property-read bool|null $reseller Is the configuration for a reseller
  */
 class Configuration extends DataSet
 {
     public static function rules(): Rules
     {
         return new Rules([
-            'api_token'  => ['required', 'string', 'min:6'],
-            'sandbox'    => ['nullable', 'boolean'],
+            'api_token' => ['required', 'string', 'min:6'],
+            'sandbox' => ['nullable', 'boolean'],
             'sharing_id' => ['nullable', 'string'],
+            'reseller' => ['nullable', 'boolean']
         ]);
+    }
+
+    public function isSandbox(): bool
+    {
+        return (bool) $this->sandbox;
+    }
+
+    public function isReseller(): bool
+    {
+        return (bool) $this->reseller;
     }
 }
